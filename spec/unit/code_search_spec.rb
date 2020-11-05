ad7286f3e1693d4c89815738346e00aaf7732637
schneems <richard.schneeman+foo@gmail.com>
2020-11-05 15:10:20 -0600

    Real world rspec example missing describe

--- spec/unit/code_search_spec.rb
+++ spec/unit/code_search_spec.rb
@@ -3,6 +3,22 @@ require_relative "../spec_helper.rb"
 module SyntaxErrorSearch
   RSpec.describe CodeSearch do
 
+    class FormatBlocks
+      def initialize(block_array)
+        @blocks = block_array
+        @lines = @blocks.map(&:lines).flatten
+        @digit_count = @lines.last.line_number.to_s.length
+      end
+
+      def to_s
+        @lines.map do |line|
+          number = line.line_number.to_s.rjust(@digit_count)
+          "#{number} #{line}"
+        end.join
+      end
+    end
+
+
     it "def with missing end" do
       search = CodeSearch.new(<<~EOM)
         class OH
@@ -23,6 +39,52 @@ module SyntaxErrorSearch
     # For code that's not perfectly formatted, we ideally want to do our best
     # These examples represent the results that exist today, but I would like to improve upon them
     describe "needs improvement" do
+      describe "missing describe/do line" do
+        it "Format Code blocks real world example" do
+          search = CodeSearch.new(<<~EOM)
+            require 'rails_helper'
+
+            RSpec.describe AclassNameHere, type: :worker do
+              describe "thing" do
+                context "when" do
+                  let(:thing) { stuff }
+                  let(:another_thing) { moarstuff }
+                  subject { foo.new.perform(foo.id, true) }
+
+                  it "stuff" do
+                    subject
+
+                    expect(foo.foo.foo).to eq(true)
+                  end
+                end
+              end
+
+                context "stuff" do
+                  let(:thing) { create(:foo, foo: stuff) }
+                  let(:another_thing) { create(:stuff) }
+
+                  subject { described_class.new.perform(foo.id, false) }
+
+                  it "more stuff" do
+                    subject
+
+                    expect(foo.foo.foo).to eq(false)
+                  end
+                end
+              end
+            end
+          EOM
+          search.call
+
+          out = FormatBlocks.new(search.invalid_blocks).to_s
+          expect(out).to eq(<<~EOM)
+             4   describe "thing" do
+            16   end
+            30   end
+          EOM
+        end
+      end
+
       describe "mis-matched-indentation" do
         it "stacked ends " do
           search = CodeSearch.new(<<~EOM)
