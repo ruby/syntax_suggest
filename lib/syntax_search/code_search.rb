149a7e5a15c9650c10be023504eb3be74110afe9
schneems <richard.schneeman+foo@gmail.com>
2020-12-04 10:50:44 -0600

    Make debugging easier

--- lib/syntax_search/code_search.rb
+++ lib/syntax_search/code_search.rb
@@ -28,7 +28,7 @@ module SyntaxErrorSearch
     private; attr_reader :frontier; public
     public; attr_reader :invalid_blocks, :record_dir, :code_lines
 
-    def initialize(source, record_dir: ENV["SYNTAX_SEARCH_RECORD_DIR"])
+    def initialize(source, record_dir: ENV["SYNTAX_SEARCH_RECORD_DIR"] || ENV["DEBUG"] ? "tmp" : nil)
       @source = source
       if record_dir
         @time = Time.now.strftime('%Y-%m-%d-%H-%M-%s-%N')
