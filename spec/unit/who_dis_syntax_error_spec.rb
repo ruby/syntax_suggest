338eb2f3c6f47ca28e152d8bfb55c4df1fb04fee
Mauro Otonelli <mauro.otonelli@gmail.com>
2021-10-12 23:58:15 -0300

    Handle mismatched end when using rescue without begin
    Handles cases where mismatched ends happens on methods using
    rescue without `begin`:
    ```
    unexpected `rescue', expecting `end'
    ```

--- spec/unit/who_dis_syntax_error_spec.rb
+++ spec/unit/who_dis_syntax_error_spec.rb
@@ -4,18 +4,41 @@ require_relative "../spec_helper"
 
 module DeadEnd
   RSpec.describe WhoDisSyntaxError do
-    it "determines the type of syntax error to be an unmatched end" do
-      expect(
-        WhoDisSyntaxError.new("def foo;").call.error_symbol
-      ).to eq(:missing_end)
+    context "determines the type of syntax error to be an unmatched end" do
+      it "with missing or extra end's" do
+        expect(
+          WhoDisSyntaxError.new("def foo;").call.error_symbol
+        ).to eq(:missing_end)
 
-      expect(
-        WhoDisSyntaxError.new("def foo; end; end").call.error_symbol
-      ).to eq(:unmatched_syntax)
+        expect(
+          WhoDisSyntaxError.new("def foo; end; end").call.error_symbol
+        ).to eq(:unmatched_syntax)
 
-      expect(
-        WhoDisSyntaxError.new("def foo; end; end").call.unmatched_symbol
-      ).to eq(:end)
+        expect(
+          WhoDisSyntaxError.new("def foo; end; end").call.unmatched_symbol
+        ).to eq(:end)
+      end
+
+      it "with unexpected rescue" do
+        source = <<~EOM
+          def foo
+            if bar
+              "baz"
+            else
+              "foo"
+          rescue FooBar
+            nil
+          end
+        EOM
+
+        expect(
+          WhoDisSyntaxError.new(source).call.error_symbol
+        ).to eq(:unmatched_syntax)
+
+        expect(
+          WhoDisSyntaxError.new(source).call.unmatched_symbol
+        ).to eq(:end)
+      end
     end
 
     context "determines the type of syntax error to be an unmatched pipe" do
