41bb011760eb53e62094afc2be0c8af89fa2b4b7
schneems <richard.schneeman+foo@gmail.com>
2022-01-14 16:55:36 -0600

    WIP docs

--- lib/dead_end/up_down_expand.rb
+++ lib/dead_end/up_down_expand.rb
@@ -1,6 +1,37 @@
 # frozen_string_literal: true
 
 module DeadEnd
+  # Expand code based on lexical heuristic
+  #
+  # Code that has unbalanced pairs cannot be valid
+  # i.e. `{` must always be matched with a `}`.
+  #
+  # This expansion class exploits that knowledge to
+  # expand a logical block towards equal pairs.
+  #
+  # For example: if code is missing a `]` it cannot
+  # be on a line above, so it must expand down
+  #
+  # This heuristic allows us to make larger and more
+  # accurate expansions which means fewer invalid
+  # blocks to check which means overall faster search.
+  #
+  # This class depends on another class LexDiff can be
+  # accesssed per-line. It holds the delta of tracked directional
+  # pairs: curly brackets, square brackets, parens, and kw/end
+  # with positive count (leaning left), 0 (balanced), or negative
+  # count (leaning right).
+  #
+  # With this lexical diff information we can look around a given
+  # block and move with inteligently. For instance if the current
+  # block has a miss matched `end` and the line above it holds
+  # `def foo` then the block will be expanded up to capture that line.
+  #
+  # An unbalanced block can never be valid (this provides info to
+  # the overall search). However a balanced block may contain other syntax
+  # error and so must be re-checked using Ripper (slow).
+  #
+  #
   class UpDownExpand
     attr_reader :start_index, :end_index
 
@@ -36,14 +67,6 @@ module DeadEnd
       CodeBlock.new(lines: @code_lines[start_index..end_index])
     end
 
-    def stop_top?
-      @start_index == 0
-    end
-
-    def stop_bottom?
-      @end_index == @last_index
-    end
-
     def balanced?
       @lex_diff.balanced?
     end
@@ -52,56 +75,6 @@ module DeadEnd
       !balanced?
     end
 
-    def direction
-      leaning = @lex_diff.leaning
-      case leaning
-      when :left # go down
-        if stop_bottom?
-          :stop
-        else
-          :down
-        end
-      when :right # go up
-        if stop_top?
-          :stop
-        else
-          :up
-        end
-      when :equal, :unknown
-        if stop_top? && stop_bottom?
-          return :stop
-        elsif stop_top? && !stop_bottom?
-          return :down
-        elsif !stop_top? && stop_bottom?
-          return :up
-        end
-        leaning
-      end
-    end
-
-    def grab_equal_or
-      did_expand = false
-      if above&.balanced?
-        did_expand = true
-        expand_up
-      end
-
-      if below&.balanced?
-        did_expand = true
-        expand_down
-      end
-
-      return true if did_expand
-
-      if above && below && above.lex_diff.leaning == :left && below.lex_diff.leaning == :right && @lex_diff.dup.concat(above.lex_diff).concat(below.lex_diff).balanced?
-        expand_up
-        expand_down
-        true
-      else
-        yield
-        false
-      end
-    end
 
     def call
       case self.direction
@@ -136,23 +109,84 @@ module DeadEnd
       self
     end
 
-    def above
+    def direction
+      leaning = @lex_diff.leaning
+      case leaning
+      when :left # go down
+        if stop_bottom?
+          :stop
+        else
+          :down
+        end
+      when :right # go up
+        if stop_top?
+          :stop
+        else
+          :up
+        end
+      when :equal, :unknown
+        if stop_top? && stop_bottom?
+          return :stop
+        elsif stop_top? && !stop_bottom?
+          return :down
+        elsif !stop_top? && stop_bottom?
+          return :up
+        end
+        leaning
+      end
+    end
+
+
+    private def grab_equal_or
+      did_expand = false
+      if above&.balanced?
+        did_expand = true
+        expand_up
+      end
+
+      if below&.balanced?
+        did_expand = true
+        expand_down
+      end
+
+      return true if did_expand
+
+      if above && below && above.lex_diff.leaning == :left && below.lex_diff.leaning == :right && @lex_diff.dup.concat(above.lex_diff).concat(below.lex_diff).balanced?
+        expand_up
+        expand_down
+        true
+      else
+        yield
+        false
+      end
+    end
+
+    private def above
       @code_lines[@start_index - 1] unless stop_top?
     end
 
-    def below
+    private def below
       @code_lines[@end_index + 1] unless stop_bottom?
     end
 
-    def expand_up
+    private def expand_up
       @start_index -= 1
       @lex_diff.concat(@code_lines[@start_index].lex_diff)
     end
 
-    def expand_down
+    private def expand_down
       @end_index += 1
       @lex_diff.concat(@code_lines[@end_index].lex_diff)
     end
+
+    private def stop_top?
+      @start_index == 0
+    end
+
+    private def stop_bottom?
+      @end_index == @last_index
+    end
+
   end
 
 end
