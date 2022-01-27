cf44254b759b8ecbcfd2d59c77fdcc1eefdf12b8
schneems <richard.schneeman+foo@gmail.com>
2022-01-27 15:42:28 -0600

    WIP rename @inner to @parents

--- spec/unit/indent_tree_spec.rb
+++ spec/unit/indent_tree_spec.rb
@@ -19,16 +19,16 @@ module DeadEnd
       end
 
       expect(tree.to_a.length).to eq(1)
-      expect(tree.root.inner.length).to eq(3)
-      expect(tree.root.inner[0].to_s).to eq(<<~'EOM')
+      expect(tree.root.parents.length).to eq(3)
+      expect(tree.root.parents[0].to_s).to eq(<<~'EOM')
         require 'ripper'
       EOM
 
-      expect(tree.root.inner[1].to_s).to eq(<<~'EOM')
+      expect(tree.root.parents[1].to_s).to eq(<<~'EOM')
         require_relative 'syntax_tree/version'
       EOM
 
-      inner = tree.root.inner[2]
+      inner = tree.root.parents[2]
       expect(inner.outer_nodes.to_s).to eq(<<~'EOM')
         class SyntaxTree < Ripper
         end
@@ -79,20 +79,20 @@ module DeadEnd
       blocks = document.to_a
       expect(blocks.length).to eq(1)
       expect(document.root.leaning).to eq(:left)
-      expect(document.root.inner[0].to_s).to eq(<<~'EOM')
+      expect(document.root.parents[0].to_s).to eq(<<~'EOM')
         class Foo
           def to_json(*opts)
             { type: :args, parts: parts, loc: location }.to_json(*opts)
           end
         end
       EOM
-      expect(document.root.inner[0].leaning).to eq(:equal)
-      expect(document.root.inner[1].inner[0].to_s).to eq(<<~'EOM')
+      expect(document.root.parents[0].leaning).to eq(:equal)
+      expect(document.root.parents[1].parents[0].to_s).to eq(<<~'EOM')
         def on_args_add(arguments, argument)
       EOM
-      expect(document.root.inner[1].inner[0].leaning).to eq(:left)
+      expect(document.root.parents[1].parents[0].leaning).to eq(:left)
 
-      expect(document.root.inner[1].inner[1].to_s).to eq(<<~'EOM'.indent(2))
+      expect(document.root.parents[1].parents[1].to_s).to eq(<<~'EOM'.indent(2))
         if arguments.parts.empty?
           Args.new(parts: [argument], location: argument.location)
         else
@@ -103,7 +103,7 @@ module DeadEnd
         end
       EOM
 
-      expect(document.root.inner[1].inner[2].to_s).to eq(<<~'EOM')
+      expect(document.root.parents[1].parents[2].to_s).to eq(<<~'EOM')
         class ArgsAddBlock
           attr_reader :arguments
           attr_reader :block
@@ -115,7 +115,7 @@ module DeadEnd
           end
         end
       EOM
-      expect(document.root.inner[1].inner[1].leaning).to eq(:equal)
+      expect(document.root.parents[1].parents[1].leaning).to eq(:equal)
     end
 
     it "valid if/else end" do
@@ -141,12 +141,12 @@ module DeadEnd
       blocks = document.to_a
       expect(blocks.length).to eq(1)
       expect(document.root.leaning).to eq(:equal)
-      expect(document.root.inner.length).to eq(3)
-      expect(document.root.inner[0].to_s).to eq(<<~'EOM')
+      expect(document.root.parents.length).to eq(3)
+      expect(document.root.parents[0].to_s).to eq(<<~'EOM')
         def on_args_add(arguments, argument)
       EOM
 
-      expect(document.root.inner[1].to_s).to eq(<<~'EOM'.indent(2))
+      expect(document.root.parents[1].to_s).to eq(<<~'EOM'.indent(2))
         if arguments.parts.empty?
           Args.new(parts: [argument], location: argument.location)
         else
@@ -157,32 +157,32 @@ module DeadEnd
         end
       EOM
 
-      expect(document.root.inner[2].to_s).to eq(<<~'EOM')
+      expect(document.root.parents[2].to_s).to eq(<<~'EOM')
         end
       EOM
 
-      inside = document.root.inner[1]
-      expect(inside.inner.length).to eq(5)
-      expect(inside.inner[0].to_s).to eq(<<~'EOM'.indent(2))
+      inside = document.root.parents[1]
+      expect(inside.parents.length).to eq(5)
+      expect(inside.parents[0].to_s).to eq(<<~'EOM'.indent(2))
         if arguments.parts.empty?
       EOM
 
-      expect(inside.inner[1].to_s).to eq(<<~'EOM'.indent(4))
+      expect(inside.parents[1].to_s).to eq(<<~'EOM'.indent(4))
           Args.new(parts: [argument], location: argument.location)
       EOM
 
-      expect(inside.inner[2].to_s).to eq(<<~'EOM'.indent(2))
+      expect(inside.parents[2].to_s).to eq(<<~'EOM'.indent(2))
         else
       EOM
 
-      expect(inside.inner[3].to_s).to eq(<<~'EOM'.indent(4))
+      expect(inside.parents[3].to_s).to eq(<<~'EOM'.indent(4))
           Args.new(
             parts: arguments.parts << argument,
             location: arguments.location.to(argument.location)
           )
       EOM
 
-      expect(inside.inner[4].to_s).to eq(<<~'EOM'.indent(2))
+      expect(inside.parents[4].to_s).to eq(<<~'EOM'.indent(2))
         end
       EOM
     end
@@ -204,24 +204,24 @@ module DeadEnd
       expect(blocks.length).to eq(1)
       expect(document.root.leaning).to eq(:right)
 
-      expect(document.root.inner.length).to eq(3)
-      expect(document.root.inner[0].to_s).to eq(<<~'EOM')
+      expect(document.root.parents.length).to eq(3)
+      expect(document.root.parents[0].to_s).to eq(<<~'EOM')
         Foo.call
       EOM
-      expect(document.root.inner[0].indent).to eq(0)
-      expect(document.root.inner[1].to_s).to eq(<<~'EOM'.indent(2))
+      expect(document.root.parents[0].indent).to eq(0)
+      expect(document.root.parents[1].to_s).to eq(<<~'EOM'.indent(2))
         def foo
           print "lol"
           print "lol"
          end # one
       EOM
-      expect(document.root.inner[1].balanced?).to be_truthy
-      expect(document.root.inner[1].indent).to eq(2)
+      expect(document.root.parents[1].balanced?).to be_truthy
+      expect(document.root.parents[1].indent).to eq(2)
 
-      expect(document.root.inner[2].to_s).to eq(<<~'EOM')
+      expect(document.root.parents[2].to_s).to eq(<<~'EOM')
         end # two
       EOM
-      expect(document.root.inner[2].indent).to eq(0)
+      expect(document.root.parents[2].indent).to eq(0)
     end
 
     it "captures complicated" do
@@ -247,20 +247,20 @@ module DeadEnd
       blocks = document.to_a
       expect(blocks.length).to eq(1)
 
-      expect(document.root.inner.length).to eq(3)
-      expect(document.root.inner[0].to_s).to eq(<<~'EOM')
+      expect(document.root.parents.length).to eq(3)
+      expect(document.root.parents[0].to_s).to eq(<<~'EOM')
         if true              # 0
           print 'huge 1'     # 1
         end                  # 2
       EOM
 
-      expect(document.root.inner[1].to_s).to eq(<<~'EOM')
+      expect(document.root.parents[1].to_s).to eq(<<~'EOM')
         if true              # 4
           print 'huge 2'     # 5
         end                  # 6
       EOM
 
-      expect(document.root.inner[2].to_s).to eq(<<~'EOM')
+      expect(document.root.parents[2].to_s).to eq(<<~'EOM')
         if true              # 8
           print 'huge 3'     # 9
         end                  # 10
@@ -296,7 +296,7 @@ module DeadEnd
       # blocks = document.to_a
       expect(document.root.to_s).to eq(code_lines.join)
       expect(document.to_a.length).to eq(1)
-      expect(document.root.inner.length).to eq(3)
+      expect(document.root.parents.length).to eq(3)
     end
 
     it "simple" do
@@ -316,7 +316,7 @@ module DeadEnd
 
       expect(search.document.root).to eq(
         BlockNode.new(lines: code_lines[0..1], indent: 0).tap { |node|
-          node.inner << BlockNode.new(lines: code_lines[0], indent: 0)
+          node.parents << BlockNode.new(lines: code_lines[0], indent: 0)
           node.right = BlockNode.new(lines: code_lines[1], indent: 0)
         }
       )
