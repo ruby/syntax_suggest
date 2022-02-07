ccd9816900ba08aa83ff804efb90a6c30fef0e7b
schneems <richard.schneeman+foo@gmail.com>
2022-02-07 16:45:04 -0600

    Document and refactor IndentTree

--- lib/dead_end/indent_tree.rb
+++ lib/dead_end/indent_tree.rb
@@ -3,6 +3,20 @@
 require_relative "block_recorder"
 
 module DeadEnd
+  # Transform a BlockDocument into a Tree
+  #
+  #   tree = IndentTree.new(document: document).call
+  #   expect(tree.root.lines).to eq(document.code_lines)
+  #
+  # Nodes are put into a queue (provided by the document)
+  # and are pulled out in a specific priority order (high coupling).
+  #
+  # A node then attempts to expand up and down according to rules here
+  # and in `BlockNode#expand_above?` and `BlockNode#expand_below?`
+  #
+  # While this process tends to produce valid code blocks from valid code
+  # it's not guaranteed. Since we will ultimately search for invalid code
+  # it's not an ideal property.
   class IndentTree
     attr_reader :document, :code_lines
 
@@ -15,46 +29,32 @@ module DeadEnd
       @recorder = BlockRecorder.from_dir(record_dir, subdir: "build_tree", code_lines: @code_lines)
     end
 
-    def to_a
-      @document.to_a
-    end
-
     def root
       @document.root
     end
 
     def call
-      reduce
-
-      self
-    end
-
-    private def reduce
       while (block = document.pop)
-        original = block
+        @recorder.capture(block, name: "pop")
+
         blocks = [block]
+        indent = block.next_indent
 
-        indent = original.next_indent
-
+        # Look up
         while blocks.last.expand_above?(with_indent: indent)
           above = blocks.last.above
-          leaning = above.leaning
-          # break if leaning == :right
           blocks << above
-          break if leaning == :left
+          break if above.leaning == :left
         end
 
         blocks.reverse!
 
+        # Look down
         while blocks.last.expand_below?(with_indent: indent)
           below = blocks.last.below
-          leaning = below.leaning
-          # break if leaning == :left
           blocks << below
-          break if leaning == :right
+          break if below.leaning == :right
         end
-
-        @recorder.capture(original, name: "pop")
 
         if blocks.length > 1
           node = document.capture_all(blocks)
