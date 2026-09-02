259d36c5cfcc247fc8dc67f0885b5897930ba021
Schneems <richard.schneeman+foo@gmail.com>
2026-09-01 20:47:35 -0500

    Add a failing test for #258

--- spec/unit/core_ext_spec.rb
+++ spec/unit/core_ext_spec.rb
@@ -28,5 +28,38 @@ module SyntaxSuggest
         expect(detailed).to_not include("print 'no newline#{original_message}")
       end
     end
+
+    # https://github.com/ruby/syntax_suggest/issues/258
+    it "SyntaxError monkeypatch does not leak raw invalid bytes into the annotation" do
+      Dir.mktmpdir do |dir|
+        tmpdir = Pathname(dir)
+        file = tmpdir.join("file.rb")
+
+        # Source contains a byte (0xFF) that is invalid in its encoding. This
+        # mirrors the invalid-encoding sources that broke Ruby's
+        # spec/ruby/language/source_encoding_spec.rb on CI machines running
+        # under LC_ALL=C.
+        file.binwrite("def foo\n  x = \xFF\nend\n")
+
+        core_ext_file = lib_dir.join("syntax_suggest").join("core_ext")
+        require_relative core_ext_file
+
+        error = SyntaxError.new("blerg")
+        def error.set_tmp_path_for_testing=(path)
+          @tmp_path_for_testing = path
+        end
+        error.set_tmp_path_for_testing = file
+        def error.path
+          @tmp_path_for_testing
+        end
+
+        detailed = error.detailed_message(highlight: false, syntax_suggest: true)
+
+        # The raw invalid bytes must not leak into the annotation. When they do,
+        # any regex match on the output raises `ArgumentError: invalid byte
+        # sequence` (e.g. under LC_ALL=C where the captured output is US-ASCII).
+        expect(detailed.valid_encoding?).to be(true)
+      end
+    end
   end
 end
